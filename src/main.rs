d5d28d4ca5558bdfaeefd9cbec75c190b22ceeff
ryo-yama <johnfun.4649.aa@gmail.com>
2024-06-22 17:15:29 +0900

    Study Basic 2024/06/22

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+//fn bubble_sort(vecTgt: Vec<i32>) {
+//    for i in 
+//}
+
 fn main() {
-    println!("Hello, world!");
+    // 基礎
+    basic();
+}
+
+fn basic() {
+    // 変数の宣言
+    let out: i32 = 712;
+
+    // ミュータブルな変数の宣言
+    let mut sample: i32 = 10;
+    println!("{}", sample);
+    sample = 20;
+    println!("{}", sample);
+
+    // リテラル
+    // 生文字リテラル:エスケープシーケンスもそのまま出力される
 }
